dd5849776bf7f2571546356966fd2438476d1bfb
Satyam Sharma <satyamsharma1725@gmail.com>
2022-10-02 03:56:39 +0530

    timepass commit

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Satyam")
+                    Greeting("Fellow")
                 }
             }
         }
@@ -38,6 +38,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     TipCalculatorTheme {
-        Greeting("Satyam")
+        Greeting("Fellow")
     }
 }
